3fb5b6dac08c83ef06063ddbff2305ae05349f34
quoctran <quoctran@kms-technology.com>
2018-09-06 17:46:17 +0700

    update

--- src/test/java/mograblog/TestSelectElement.java
+++ src/test/java/mograblog/TestSelectElement.java
@@ -2,6 +2,7 @@ package mograblog;
 
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 
@@ -15,11 +16,13 @@ public class TestSelectElement {
     @Test
     public void shouldHave2Options(){
 
-        WebDriver firefoxDriver = new FirefoxDriver();
-        firefoxDriver.get("http://www.mograblog.com/2013/08/extending-selenium-in-java.html");
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("http://www.mograblog.com/2013/08/extending-selenium-in-java.html");
         MyPage page = new MyPage();
 
-        PageFactory.initElements( new MograblogFieldDecorator( firefoxDriver, firefoxDriver ), page );
+        PageFactory.initElements( new MograblogFieldDecorator( chromeDriver, chromeDriver ), page );
         page.changeToOption2();
 
         try{ // normally we don't need this, but we want to see it change this time, so I added sleep
